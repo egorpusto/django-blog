b9cfea0848487bcbff09c355afc5fdeee0391f3d
Egor Anoshin <egor_pusto@icloud.com>
2026-03-06 23:51:14 +0300

    refactor: rewrite views to Class-Based Views (ListView, DetailView, FormView)

--- blog/urls.py
+++ blog/urls.py
@@ -1,42 +1,40 @@
 from django.urls import path
+
 from . import views
 from .feeds import LatestPostsFeed
 
-
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    # Views using class-based views
-    # path("", views.PostListView.as_view(), name='post_list'),
-    path("", views.post_list, name='post_list'),
+    path("", views.PostListView.as_view(), name="post_list"),
     path(
-        'tag/<slug:tag_slug>/',
-        views.post_list,
-        name='post_list_by_tag'
+        "tag/<slug:tag_slug>/",
+        views.PostListView.as_view(),
+        name="post_list_by_tag",
     ),
     path(
-        '<int:year>/<int:month>/<int:day>/<slug:post>/',
-          views.post_detail, 
-          name='post_detail'
-          ),
-    path(
-        'share/<int:post_id>/',
-        views.post_share,
-        name='post_share'
+        "<int:year>/<int:month>/<int:day>/<slug:post>/",
+        views.PostDetailView.as_view(),
+        name="post_detail",
     ),
     path(
-        'comment/<int:post_id>/',
+        "share/<int:post_id>/",
+        views.PostShareView.as_view(),
+        name="post_share",
+    ),
+    path(
+        "comment/<int:post_id>/",
         views.post_comment,
-        name='post_comment'
+        name="post_comment",
     ),
     path(
-        'feed/',
+        "feed/",
         LatestPostsFeed(),
-        name='post_feed'
+        name="post_feed",
     ),
     path(
-        'search/',
+        "search/",
         views.post_search,
-        name='post_search'
+        name="post_search",
     ),
 ]
